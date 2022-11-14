be941d40ed849eaaf76b5e6c8a470d53852f7557
ksmithut <github@ksmithut.com>
2022-11-14 08:47:02 -0700

    2015 day 2

---
+++ lib/2015/02.ex
@@ -0,0 +1,68 @@
+import AdventOfCode
+
+solution 2015, 2 do
+  @moduledoc """
+  https://adventofcode.com/2015/day/2
+  https://adventofcode.com/2015/day/2/input
+  """
+
+  @doc ~S"""
+      iex> "2x3x4" |> part_1()
+      58
+
+      iex> "1x1x10" |> part_1()
+      43
+
+      iex> input() |> part_1()
+      1606483
+  """
+  def part_1(input) do
+    input
+    |> parse()
+    |> Enum.map(&wrapping_paper/1)
+    |> Enum.sum()
+  end
+
+  defp parse(input) do
+    input
+    |> String.split()
+    |> Enum.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split("x")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp wrapping_paper([l, w, h]) do
+    areas = [l * w, l * h, w * h]
+    Enum.sum(areas) * 2 + Enum.min(areas)
+  end
+
+  @doc ~S"""
+      iex> "2x3x4" |> part_2()
+      34
+
+      iex> "1x1x10" |> part_2()
+      14
+
+      iex> input() |> part_2()
+      3842356
+  """
+  def part_2(input) do
+    input
+    |> parse()
+    |> Enum.map(&ribbon/1)
+    |> Enum.sum()
+  end
+
+  defp ribbon(sides) do
+    sides
+    |> Enum.sort(:asc)
+    |> Enum.take(2)
+    |> Enum.sum()
+    |> Kernel.*(2)
+    |> Kernel.+(Enum.product(sides))
+  end
+end
--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -3,6 +3,7 @@ defmodule AdventOfCodeTest do
   import AdventOfCode.TestHelper
 
   advent_test 2015, 1
+  advent_test 2015, 2
 
   advent_test 2016, 1
 
