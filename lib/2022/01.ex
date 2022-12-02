054da139ba0638bc56fefb778a97c65de09bf1f0
ksmithut <github@ksmithut.com>
2022-12-01 21:54:31 -0700

    2022 day 1

---
+++ lib/2022/01.ex
@@ -0,0 +1,63 @@
+import AdventOfCode
+
+solution 2022, 1 do
+  @moduledoc """
+  https://adventofcode.com/2022/day/1
+  https://adventofcode.com/2022/day/1/input
+  """
+
+  def sample do
+    """
+    1000
+    2000
+    3000
+
+    4000
+
+    5000
+    6000
+
+    7000
+    8000
+    9000
+
+    10000
+    """
+  end
+
+  @doc ~S"""
+      iex> sample() |> part_1()
+      24000
+
+      iex> input() |> part_1()
+      70369
+  """
+  def part_1(input) do
+    sum_top(input, 1)
+  end
+
+  defp sum_top(input, top) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(fn elf ->
+      elf
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.sort(:desc)
+    |> Enum.take(top)
+    |> Enum.sum()
+  end
+
+  @doc ~S"""
+      iex> sample() |> part_2()
+      45000
+
+      iex> input() |> part_2()
+      203002
+  """
+  def part_2(input) do
+    sum_top(input, 3)
+  end
+end
--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -2,18 +2,20 @@ defmodule AdventOfCodeTest do
   use ExUnit.Case
   import AdventOfCode.TestHelper
 
-  advent_test 2015, 1
-  advent_test 2015, 2
+  # advent_test 2015, 1
+  # advent_test 2015, 2
 
-  advent_test 2016, 1
+  # advent_test 2016, 1
 
-  advent_test 2017, 1
+  # advent_test 2017, 1
 
-  advent_test 2018, 1
+  # advent_test 2018, 1
 
-  advent_test 2019, 1
+  # advent_test 2019, 1
 
-  advent_test 2020, 1
+  # advent_test 2020, 1
 
-  advent_test 2021, 1
+  # advent_test 2021, 1
+
+  advent_test 2022, 1
 end
